c8209c4ee7c37d7a4b2780ae2cc9747ce0579d8f
NXTGenforcer <gskywalker11@gmail.com>
2022-01-04 22:39:00 -0500

    Finished product

--- index.js
+++ index.js
@@ -2,73 +2,63 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const teamArr = [];
 const cardArr = [];
-// const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
-let teamDiv = ``;
 
-function init() {
-  return inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "EmployeeRole",
-        choices: ["Engineer", "Manager", "Intern"],
-      },
-      {
-        type: "input",
-        name: "name",
+async function init() {
+  const { name, email, id, EmployeeRole } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "EmployeeRole",
+      choices: ["Engineer", "Manager", "Intern"],
+    },
+    {
+      type: "input",
+      name: "name",
 
-        message: "What is the employee's name?",
-      },
-      {
-        type: "input",
-        name: "email",
+      message: "What is the employee's name?",
+    },
+    {
+      type: "input",
+      name: "email",
 
-        message: "What is the employee's email?",
-      },
-      {
-        type: "input",
-        name: "id",
+      message: "What is the employee's email?",
+    },
+    {
+      type: "input",
+      name: "id",
 
-        message: "What is the employee's Id number?",
-      },
-    ])
-    .then(async ({ name, email, id, EmployeeRole }) => {
-      if (EmployeeRole === "Engineer") {
-        const { github } = await EngineerInfo();
-        const newEngineer = new Engineer(name, email, id, github);
-        teamArr.push(newEngineer);
-      } else if (EmployeeRole === "Manager") {
-        const { officeNum } = await ManagerInfo();
-        const newManager = new Manager(name, email, id, officeNum);
-        teamArr.push(newManager);
-      } else {
-        const { school } = await InternInfo();
-        const newIntern = new Intern(name, email, id, school);
-        teamArr.push(newIntern);
-      }
-      const { aMember } = await newMember();
-      if (aMember) {
-        return init();
-      }
-    })
-    .then(async () => {
-      for (let i = 0; i < teamArr.length; i++) {
-        if (teamArr[i].getRole() === "Engineer") {
-          engineerCard(teamArr[i]);
-        } else if (teamArr[i].getRole() === "Manager") {
-          managerCard(teamArr[i]);
-        } else {
-          internCard(teamArr[i]);
-        }
-      }
-      console.log(cardArr);
-      teamDiv = teamDiv.concat(cardArr.join(""));
-      console.log(teamDiv);
-      writeToFile();
-    });
+      message: "What is the employee's Id number?",
+    },
+  ]);
+  if (EmployeeRole === "Engineer") {
+    const { github } = await EngineerInfo();
+    const newEngineer = new Engineer(name, email, id, github);
+    teamArr.push(newEngineer);
+  } else if (EmployeeRole === "Manager") {
+    const { officeNum } = await ManagerInfo();
+    const newManager = new Manager(name, email, id, officeNum);
+    teamArr.push(newManager);
+  } else {
+    const { school } = await InternInfo();
+    const newIntern = new Intern(name, email, id, school);
+    teamArr.push(newIntern);
+  }
+  const { aMember } = await newMember();
+  if (aMember) {
+    return init();
+  }
+  for (let i = 0; i < teamArr.length; i++) {
+    if (teamArr[i].getRole() === "Engineer") {
+      engineerCard(teamArr[i]);
+    } else if (teamArr[i].getRole() === "Manager") {
+      managerCard(teamArr[i]);
+    } else {
+      internCard(teamArr[i]);
+    }
+  }
+  writeToFile();
 }
 
 function EngineerInfo() {
@@ -112,45 +102,45 @@ const newMember = () => {
 };
 
 function engineerCard({ name, email, id, github }) {
-  const engineerCard = `<div class="card engineer-card" style="width: 12rem;">
+  const engineerCard = `<div class="card engineer-card p-0" style="width: 12rem;">
       <div class="card-header bg-primary">
           <h5 class="card-name text-light">${name}</h5>
-          <i class="fa-solid fa-mug-hot fa-lg text-light">Manager</i>
+          <i class="fas fa-glasses light text-light">Engineer</i>
       </div>
       <div class="card-body">
-          <li class="list-group-item id">ID:${id}</li>
-          <li class="list-group-item email">Email:${email}</li>
-          <li class="list-group-item office-num">Office Number:${github}</li>
+          <li class="list-group-item id">ID: ${id}</li>
+          <li class="list-group-item email">Email: ${email}</li>
+          <li class="list-group-item github">Github: ${github}</li>
       </div>
   </div>`;
   cardArr.push(engineerCard);
 }
 
-function managerCard({ name, email, id, officeNum }) {
-  const managerCard = `<div class="card manager-card" style="width: 12rem;">
+function managerCard({ name, email, id, officeNumber }) {
+  const managerCard = `<div class="card manager-card p-0" style="width: 12rem;">
       <div class="card-header bg-primary">
           <h5 class="card-name text-light">${name}</h5>
-          <i class="fa-solid fa-mug-hot fa-lg text-light">Manager</i>
+          <i class="fas fa-mug-hot fa-lg light text-light">Manager</i>
       </div>
       <div class="card-body">
-          <li class="list-group-item id">ID:${id}</li>
-          <li class="list-group-item email">Email:${email}</li>
-          <li class="list-group-item office-num">Office Number:${officeNum}</li>
+          <li class="list-group-item id">ID: ${id}</li>
+          <li class="list-group-item email">Email: ${email}</li>
+          <li class="list-group-item office-num">Office Number: ${officeNumber}</li>
       </div>
   </div>`;
   cardArr.push(managerCard);
 }
 
 function internCard({ name, email, id, school }) {
-  const internCard = `<div class="card intern-card" style="width: 12rem;">
+  const internCard = `<div class="card intern-card p-0" style="width: 12rem;">
       <div class="card-header bg-primary">
           <h5 class="card-name text-light">${name}</h5>
-          <i class="fa-solid fa-mug-hot fa-lg text-light">Manager</i>
+          <i class="fas fa-user-graduate light text-light">Intern</i>
       </div>
       <div class="card-body">
-          <li class="list-group-item id">ID:${id}</li>
-          <li class="list-group-item email">Email:${email}</li>
-          <li class="list-group-item office-num">Office Number:${school}</li>
+          <li class="list-group-item id">ID: ${id}</li>
+          <li class="list-group-item email">Email: ${email}</li>
+          <li class="list-group-item school">School: ${school}</li>
       </div>
   </div>`;
   cardArr.push(internCard);
@@ -167,7 +157,8 @@ function html() {
       <title>Team builder</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/font-awesome.min.css">
+          <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" 
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
   </head>
   
   <body>
@@ -176,7 +167,9 @@ function html() {
               <h1 class="text-light">My Team</h1>
           </li>
       </ul>
-      <div class="container justify-content-evenly row">${teamDiv}</div>
+      <div class="container-fluid justify-content-evenly row mt-4">${cardArr.join(
+        ""
+      )}</div>
     </body>`;
 }
 
